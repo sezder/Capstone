dafd000b1a7e33bb47511a25fd5d1dd56020bd58
sezder <84882854+sezder@users.noreply.github.com>
2022-01-19 11:35:10 -0700

    tweaks to user auth

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [iconURL, setIconURL] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
@@ -17,17 +20,29 @@ const SignUpForm = () => {
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, password));
       if (data) {
-        setErrors(data)
+        setErrors(data);
       }
     }
   };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
+  const updateFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const updateLastName = (e) => {
+    setLastName(e.target.value);
   };
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
+  };
+
+  const updateJobTitle = (e) => {
+    setJobTitle(e.target.value);
+  };
+
+  const updateIconURL = (e) => {
+    setIconURL(e.target.value);
   };
 
   const updatePassword = (e) => {
@@ -39,7 +54,7 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
@@ -49,44 +64,63 @@ const SignUpForm = () => {
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
+
+      <input
+        placeholder="First Name"
+        type="text"
+        name="firstName"
+        onChange={updateFirstName}
+        value={firstName}
+      ></input>
+
+      <input
+        placeholder="Last Name"
+        type="text"
+        name="lastName"
+        onChange={updateLastName}
+        value={lastName}
+      ></input>
+
+      <input
+        placeholder="Email"
+        type="text"
+        name="email"
+        onChange={updateEmail}
+        value={email}
+      ></input>
+
+      <input
+        placeholder="Job Title"
+        type="text"
+        name="jobTitle"
+        onChange={updateJobTitle}
+        value={jobTitle}
+      ></input>
+
+      <input
+        placeholder="Profile Photo URL (optional)"
+        type="text"
+        name="iconURL"
+        onChange={updateIconURL}
+        value={iconURL}
+      ></input>
+
+      <input
+        type="password"
+        name="password"
+        onChange={updatePassword}
+        value={password}
+      ></input>
+
+      <input
+        type="password"
+        name="repeat_password"
+        onChange={updateRepeatPassword}
+        value={repeatPassword}
+        required={true}
+      ></input>
+
+      <button type="submit">Sign Up</button>
     </form>
   );
 };
