ddd555403ecbfc6ffc0f345c17b1d182e4eb0ec2
sezder <84882854+sezder@users.noreply.github.com>
2022-01-20 10:38:48 -0700

    flask form

--- app/forms/todo_form.py
+++ app/forms/todo_form.py
@@ -4,4 +4,8 @@ from wtforms.validators import DataRequired
 from app.models import Todo
 
 class TodoForm(FlaskForm):
-  pass
+  task = StringField('task', validators=[DataRequired()])
+  list_id = IntegerField('list_id', validators=[DataRequired()])
+  creator_id = IntegerField('creator_id', validators=[DataRequired()])
+  completed = BooleanField('created', validators=[DataRequired()])
+  due = DateField('due', validators=[DataRequired()])
