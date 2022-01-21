48a1271bc00272256d81b25d394a0cac75b7f358
sezder <84882854+sezder@users.noreply.github.com>
2022-01-20 21:50:12 -0700

    navbar and footer styling

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -3,16 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../Auth/LogoutButton";
 import DemoButton from "../DemoButton";
+import "./NavBar.css";
 
 const NavBar = () => {
   const user = useSelector((state) => state.session.user);
 
-
   let sessionLinks;
   if (user) {
     sessionLinks = (
-      <div className="auth_nav_div">
-        <div className="auth_user_info">
+      <>
+        <div>
           {/* <div className="auth_user_icon">
             {user?.icon_url ? null : user?.username[0]}
             {user?.icon_url && <img src={user?.icon_url} alt="user icon"></img>}
@@ -20,78 +20,57 @@ const NavBar = () => {
           <p>{user?.username}</p>
         </div>
         <LogoutButton />
-      </div>
+      </>
     );
   } else {
     sessionLinks = (
       <>
-        <div className="auth_nav_div">
-          <li>
-            <button>
-              <NavLink
-                to="/login"
-                exact={true}
-                activeClassName="active"
-                className="light_small"
-              >
-                Login
-              </NavLink>
-            </button>
-          </li>
-          <li>
-            <button>
-              <NavLink
-                to="/sign-up"
-                exact={true}
-                activeClassName="active"
-                className="light_small"
-              >
-                Sign Up
-              </NavLink>
-            </button>
-          </li>
-          <li>
-            <DemoButton />
-          </li>
-        </div>
+        <button>
+          <NavLink
+            to="/login"
+            exact={true}
+            activeClassName="active"
+            className="light_small"
+          >
+            Login
+          </NavLink>
+        </button>
+
+        <button>
+          <NavLink
+            to="/sign-up"
+            exact={true}
+            activeClassName="active"
+            className="light_small"
+          >
+            Sign Up
+          </NavLink>
+        </button>
+
+        <DemoButton />
       </>
     );
   }
   return (
     <nav>
-      <ul>
-        {/* Logo */}
-        <NavLink to="/">
-          <div className="logo_div">
-            <h1 id="logo">
-              The Hub
-            </h1>
-          </div>
-        </NavLink>
+      {/* Logo */}
+      <NavLink to="/">
+        <h1 id="logo">HQ</h1>
+      </NavLink>
 
-        {/* Navigation Links */}
-        <div className="nav_links">
-          <li>
-            <NavLink
-              to={`/projects`}
-              exact={true}
-            >
-              Projects
-            </NavLink>
-          </li>
-          <li>
-          </li>
+      {/* Navigation Links */}
 
-          <li>
-            <a href="https://github.com/sezder/The-Hub">
-              <i className="fab fa-github fa-2x"></i>
-            </a>
-          </li>
-        </div>
+      <NavLink to={`/projects`} exact={true}>
+        Projects
+      </NavLink>
 
+      <div className="auth_links"> 
+        <a href="https://github.com/sezder/The-Hub">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
         {/* Authentication */}
         {sessionLinks}
-      </ul>
+      </div>
     </nav>
   );
 };
