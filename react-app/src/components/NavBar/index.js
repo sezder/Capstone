4ba40c4b41914c13f47ebda14b57fb2451a1bc51
sezder <84882854+sezder@users.noreply.github.com>
2022-01-21 11:57:34 -0700

    user navbar

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -3,22 +3,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../Auth/LogoutButton";
 import DemoButton from "../DemoButton";
+import { formatName, formatInitials } from "../../helperFuncs";
 import "./NavBar.css";
 
 const NavBar = () => {
   const user = useSelector((state) => state.session.user);
+  console.log(user);
 
   let sessionLinks;
   if (user) {
     sessionLinks = (
       <>
-        <div>
-          {/* <div className="auth_user_icon">
-            {user?.icon_url ? null : user?.username[0]}
+        <div className="user_info_container">
+          <div className="auth_user_icon">
+            {/* If they don't have an icon, display their first and last initials */}
+            {user?.icon_url
+              ? null
+              : formatInitials(user?.first_name, user?.last_name)}
+
+            {/* Otherwise display the icon */}
             {user?.icon_url && <img src={user?.icon_url} alt="user icon"></img>}
-          </div> */}
-          <p>{user?.username}</p>
+          </div>
+          <h2>{formatName(user?.first_name, user?.last_name)}</h2>
         </div>
+
         <LogoutButton />
       </>
     );
@@ -55,7 +63,10 @@ const NavBar = () => {
     <nav>
       {/* Logo */}
       <NavLink to="/">
-        <h1 id="logo">HQ</h1>
+        <div className="logo_container">
+          <div className="border"></div>
+          <h1 id="logo">HQ</h1>
+        </div>
       </NavLink>
 
       {/* Navigation Links */}
@@ -64,7 +75,7 @@ const NavBar = () => {
         Projects
       </NavLink>
 
-      <div className="auth_links"> 
+      <div className="auth_links">
         <a href="https://github.com/sezder/The-Hub">
           <i className="fab fa-github fa-2x"></i>
         </a>
