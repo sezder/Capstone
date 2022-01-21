b12c8b6284c0fc80ee9deed4d9e5a364b835499b
sezder <84882854+sezder@users.noreply.github.com>
2022-01-20 22:30:32 -0700

    card layout working

--- react-app/src/components/ShowProjects/index.js
+++ react-app/src/components/ShowProjects/index.js
@@ -14,15 +14,13 @@ const ShowProjects = () => {
   }, [dispatch]);
 
   return (
-    <main id="projects_grid">
+    <main>
       <section className="cards">
         {Object.values(projects).map((project, idx) => {
           return (
-            <NavLink to={`/projects/${project?.id}`} key={idx}>
-              <div className="card">
+            <NavLink to={`/projects/${project?.id}`} key={idx} className="card">
                 <h1>{project?.name}</h1>
                 <p>{project?.description}</p>
-              </div>
             </NavLink>
           );
         })}
