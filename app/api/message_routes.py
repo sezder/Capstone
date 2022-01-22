d7c687e23035d83338b3348a30e3753d65a629fa
sezder <84882854+sezder@users.noreply.github.com>
2022-01-22 16:46:27 -0700

    create a new message

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -15,21 +15,22 @@ def messages_for_project(message_id):
 
 
 # # ~~~~~~~~~~~ Create a new list ~~~~~~~~~~~ 
-# @list_routes.route('/', methods=['POST'])
-# @login_required
-# def new_list():
-#   form = ListForm()
-#   form['csrf_token'].data = request.cookies['csrf_token']
+@message_routes.route('/', methods=['POST'])
+@login_required
+def new_message():
+  form = MessageForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
 
-#   if form.validate_on_submit():
-#     list = List(
-#       title=form.data['title'],
-#       description=form.data['description'],
-#       project_id=form.data['project_id'],
-#       creator_id=form.data['creator_id']
-#     )
-#     db.session.add(list)
-#     db.session.commit()
-#     return list.to_dict()
+  if form.validate_on_submit():
+    message = List (
+      subject_line = form.data['subject_line'],
+      content = form.data['content'],
+      project_id=form.data['project_id'],
+      creator_id=form.data['creator_id']
+    )
+    
+    db.session.add(message)
+    db.session.commit()
+    return message.to_dict()
 
-#   return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 400
