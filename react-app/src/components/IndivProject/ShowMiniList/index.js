b19e4f53cfe847b9809f129c964bdbea107073cc
sezder <84882854+sezder@users.noreply.github.com>
2022-01-21 19:26:52 -0700

    thunk for get all tasks

--- react-app/src/components/IndivProject/ShowMiniList/index.js
+++ react-app/src/components/IndivProject/ShowMiniList/index.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
-import { getTodos } from "../../../store/todo";
+// import { getAllTodos } from "../../../store/todo";
 import "./ShowMiniList.css";
 
 const ShowMiniList = ({ list }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getTodos(list?.id));
+    // dispatch(getAllTodos());
   }, [dispatch]);
 
   const tasks = useSelector((state) => state.todos);
+  const tasksByList = Object.values(tasks).filter(
+    (task) => task?.list_id === list?.id
+  );
 
   return (
     <div>
