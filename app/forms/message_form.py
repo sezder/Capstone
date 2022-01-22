e6a7d98cce6fb1c7f53bbe384658adea110fb5a0
sezder <84882854+sezder@users.noreply.github.com>
2022-01-22 15:38:19 -0700

    skeleton for MessageForm

---
+++ app/forms/message_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+from app.models import Message
+
+class MessageForm(FlaskForm):
+  pass
+  # name = StringField('name', validators=[DataRequired()])
+  # description = StringField('description')
+  # creator_id = IntegerField('creator_id', validators=[DataRequired()])
