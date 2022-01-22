195ad5eab376577af6d1ba1df28d70adf9980013
sezder <84882854+sezder@users.noreply.github.com>
2022-01-22 16:41:46 -0700

    message form

--- app/forms/message_form.py
+++ app/forms/message_form.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, DateField
 from wtforms.validators import DataRequired
 from app.models import Message
 
 class MessageForm(FlaskForm):
-  pass
-  # name = StringField('name', validators=[DataRequired()])
-  # description = StringField('description')
-  # creator_id = IntegerField('creator_id', validators=[DataRequired()])
+  subject_line = StringField('subject_line', validators=[DataRequired()])
+  content = StringField('content', validators=[DataRequired()])
+  project_id = IntegerField('project_id', validators=[DataRequired()])
+  creator_id = IntegerField('creator_id', validators=[DataRequired()])
+  created_at = DateField('created_at')
+  updated_at = DateField('updated_at')
