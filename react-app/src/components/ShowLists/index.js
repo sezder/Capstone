5bac7ad61dde38b430390b6d1101b664641efb23
sezder <84882854+sezder@users.noreply.github.com>
2022-01-24 16:24:46 -0700

    todos now on cards

--- react-app/src/components/ShowLists/index.js
+++ react-app/src/components/ShowLists/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
+import { getAllTodos } from "../../store/todo";
 import { getLists } from "../../store/list";
 import "./ShowLists.css";
+import PreviewTodos from "./PreviewTodos";
 
 const ShowLists = () => {
   let { projectId } = useParams();
@@ -10,6 +12,11 @@ const ShowLists = () => {
   const dispatch = useDispatch();
 
   const lists = useSelector((state) => state.lists);
+
+  
+  useEffect(() => {
+    dispatch(getAllTodos());
+  }, [dispatch]);
 
   useEffect(() => {
     dispatch(getLists(projectId));
@@ -19,10 +26,16 @@ const ShowLists = () => {
       <section className="cards">
         {Object.values(lists).map((list, idx) => {
           return (
-            <NavLink to={`/projects/${projectId}/lists/${list?.id}`} key={idx}>
-              <div className="card">
+            <NavLink
+              to={`/projects/${projectId}/lists/${list?.id}`}
+              key={idx}
+              className="card"
+            >
+              <div>
                 <h1>{list?.title}</h1>
                 <p>{list?.description}</p>
+
+                <PreviewTodos list={list}/>
               </div>
             </NavLink>
           );
