3c09954eaa65eaaf505309544c2b51ec66b9573a
sezder <84882854+sezder@users.noreply.github.com>
2022-01-27 11:08:01 -0700

    proj assignments

--- app/seeds/users_projects.py
+++ app/seeds/users_projects.py
@@ -1,7 +1,18 @@
 from app.models import db, User_Project
 
 seeded_users_projects = [
+  User_Project(user_id=4, project_id=1),
   User_Project(user_id=1, project_id=1),
+  User_Project(user_id=2, project_id=1),
+  User_Project(user_id=7, project_id=1),
+  User_Project(user_id=11, project_id=1),
+
+  User_Project(user_id=3, project_id=2),
+  User_Project(user_id=6, project_id=2),
+  User_Project(user_id=8, project_id=2),
+
+  User_Project(user_id=8, project_id=2),
+  User_Project(user_id=7, project_id=2),
 # User_Project(user_id=, project_id=),
 ]
 
