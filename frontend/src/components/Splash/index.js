bb3dfac1ebfd701ff1c366a061d81118deba28c7
sezder <84882854+sezder@users.noreply.github.com>
2022-01-27 12:45:07 -0700

    styled splash page

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "./Splash.css";
+import { NavLink } from "react-router-dom";
 import work_together from "../images/work_together.svg";
 
 const Splash = () => {
   return (
-    <main>
-      <img src={work_together} alt="Graphic of two individuals collaborating."></img>
+    <main className="splash_main">
+      <section className="splash_img_sec">
+        <img
+          src={work_together}
+          alt="Graphic of two individuals collaborating."
+        ></img>
+      </section>
+      <section className="splash_text_sec">
+        <div>
+          <h1 className="light_large">
+            The All-In-One Toolkit<br></br> for Working Remotely.
+          </h1>
+          <p>
+            <strong>Before Headquarters:</strong> Projects feel scattered,
+            things slip, it's tough to see where things stand, and people are
+            stressed.
+          </p>
+          <p>
+            <strong>After Headquarters:</strong> Everything's organized in one
+            place, you're on top of things, progress is clear, and a sense of
+            calm sets in.
+          </p>
+          <NavLink to="/sign-up">
+            <button>Give HQ a Try</button>
+          </NavLink>
+        </div>
+      </section>
     </main>
   );
 };
